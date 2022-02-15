ac89c99f07628dc34a563cdf454cfc51b820ec4a
Sasha Czerniawski <sashacandoit@gmail.com>
2022-02-14 19:43:19 -0500

    done

--- 19_friend_date/friend_date.py
+++ 19_friend_date/friend_date.py
@@ -4,7 +4,7 @@ def friend_date(a, b):
     - a: friend #1, a tuple of (name, age, list-of-hobbies)
     - b: same, for friend #2
 
-    Returns True if they have any hobbies in common, False is not.
+    Returns True if they have any hobbies in common, False if not.
 
         >>> elmo = ('Elmo', 5, ['hugging', 'being nice'])
         >>> sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
@@ -16,3 +16,17 @@ def friend_date(a, b):
         >>> friend_date(sauron, gandalf)
         True
     """
+
+    # retrieve the list of hobbies from each friend and convert to a set
+    # find the intersection of the sets, and if any return True, else return False
+
+    if set(a[2]) & set(b[2]):
+        return True
+    else:
+        return False
+
+
+elmo = ('Elmo', 5, ['hugging', 'being nice'])
+sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+
