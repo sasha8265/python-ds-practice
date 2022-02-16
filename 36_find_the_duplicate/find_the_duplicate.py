b143255a4079742233fe51570448dd03aa901e8e
Sasha Czerniawski <sashacandoit@gmail.com>
2022-02-15 20:37:30 -0500

    done

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,12 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+
+    checked_nums = set()
+
+    for num in nums:
+        if num in checked_nums:
+            return num
+        else:
+            checked_nums.add(num)
+    return None
